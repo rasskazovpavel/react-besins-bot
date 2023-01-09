16a15a4f00b49520ac2321d98424e02d454c832f
Tolya Fam <a.fam@onpoint.ru>
2023-01-09 17:59:48 +0300

    change reg button

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,18 +1,18 @@
-import React, { useState, useEffect, useCallback } from "react";
-import isEmail from "validator/es/lib/isEmail";
-import isMobilePhone from "validator/es/lib/isMobilePhone";
-import Title from "../../components/Title/Title.jsx";
-import Input from "../../components/Input/Input.jsx";
-import Checkbox from "../../components/Checkbox/Checkbox.jsx";
+import React, { useState, useEffect, useCallback } from 'react';
+import isEmail from 'validator/es/lib/isEmail';
+import isMobilePhone from 'validator/es/lib/isMobilePhone';
+import Title from '../../components/Title/Title.jsx';
+import Input from '../../components/Input/Input.jsx';
+import Checkbox from '../../components/Checkbox/Checkbox.jsx';
 
-import useFormValidation from "../../hooks/useFormValidation";
+import useFormValidation from '../../hooks/useFormValidation';
+import { useTelegram } from '../../hooks/useTelegram.js';
 
-import "./Registration.css";
-import { useTelegram } from "../../hooks/useTelegram.js";
+import './Registration.css';
 
 const NUMBER_INPUT_COUNT = 5;
 
-function Registration() {
+export default function Registration() {
   const [check, setCheck] = useState(false);
   const { handleChange, values, setValues } = useFormValidation();
   const { tg } = useTelegram();
@@ -23,30 +23,44 @@ function Registration() {
   }, [values, tg]);
 
   useEffect(() => {
-    tg.onEvent("mainButtonClicked", onSendData);
+    tg.onEvent('mainButtonClicked', onSendData);
     return () => {
-      tg.offEvent("mainButtonClicked", onSendData);
+      tg.offEvent('mainButtonClicked', onSendData);
     };
   }, [onSendData, tg]);
 
   useEffect(() => {
+    tg.MainButton.show();
     tg.MainButton.setParams({
-      text: "Зарегистрироваться",
-      color: "#4e5994",
+      text: 'Зарегистрироваться',
+      color: '#4e5994',
     });
   }, [tg.MainButton]);
 
   useEffect(() => {
     const checkValidity = () => {
       return (
+        Object.values(values).includes('') &&
         Object.keys(values).length === NUMBER_INPUT_COUNT &&
         check &&
-        isEmail(values["mail"]) &&
-        isMobilePhone(values["phone"], "ru-RU")
+        isEmail(values['mail']) &&
+        isMobilePhone(values['phone'], 'ru-RU')
       );
     };
 
-    checkValidity() ? tg.MainButton.show() : tg.MainButton.hide();
+    if (checkValidity()) {
+      tg.MainButton.enable();
+      tg.MainButton.setParams({
+        text: 'Зарегистрироваться',
+        color: '#4e5994',
+      });
+    } else {
+      tg.MainButton.disable();
+      tg.MainButton.setParams({
+        text: 'Зарегистрироваться',
+        color: '#858585',
+      });
+    }
   }, [values, tg.MainButton, check]);
 
   return (
@@ -89,7 +103,7 @@ function Registration() {
             values={values}
             setValues={setValues}
             type="tel"
-            placeholder={"Формат: 89997776655"}
+            placeholder={'Формат: 89997776655'}
           />
           <Input
             label="Почта"
@@ -107,24 +121,7 @@ function Registration() {
             value={check}
           />
         </div>
-        {/* <div className="registration__footer">
-            <Button valid={isFormValid && !isZeroInInputs}>
-              Зарегистрироваться
-            </Button>
-            <Button
-              valid={Object.values(values).length !== 0}
-              handler={() => {
-                setValues({});
-                setIsFormValid(false);
-              }}
-              type="button"
-            >
-              Очистить
-            </Button>
-          </div> */}
       </form>
     </>
   );
 }
-
-export default Registration;
